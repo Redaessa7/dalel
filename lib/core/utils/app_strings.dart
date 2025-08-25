a6ce4cd87adac92da3878a1d74edb498e3d8e0fe
Redaessa7 <redaesssa@gmail.com>
2025-08-25 16:19:27 +0300

    add app strings

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -1,21 +1,31 @@
 abstract class AppStrings {
-  static const String appName = "Dalel";
-  static const String next = "Next";
-  static const String skip = "Skip";
-  static const String createAccount = "Create Account";
-  static const String signUp = "Sign Up";
-  static const String signIn = "Sign In";
-  static const String alreadyHaveAnAccount = "Already have an account ?";
-  static const String forgotPassword = "Forgot Password";
-  static const String verifyAccount = "Verify Account";
-  static const String searchHistory = "Search deep into the history";
-  static const String profile = "Profile";
-  static const String editProfile = "Edit Profile";
-  static const String notification = "Notification";
-  static const String settings = "Settings";
-  static const String general = "General";
-  static const String account = "Account";
-  static const String logOut = "Log Out";
-  static const String privacyPolicy = "Privacy Policy";
-  static const String security = "Security";
+  //ToDo:All Strings
+
+  static const String appName = "Dalel",
+  //!On Boarding Strings
+  skip = "Skip", next = "Next", createAccount = "Create Account", loginNow = "Login Now",
+  //!Auth Strings
+  welcome = "Welcome!", welcomeBack = "Welcome Back!", fristName = "First Name", lastName = "Last Name", emailAddress = "Email Address", password = "Password", iHaveAgreeToOur = "I have agree to our ", termsAndCondition = "Terms and Condition", signUp = "Sign Up", signIn = "Sign In", alreadyHaveAnAccount = "Already have an account ? ", dontHaveAnAccount = "Don’t have an account ? ", forgotPassword = "Forgot Password ?",
+  //!Home Strings
+  recommendations = "Recommendations", about = "About", wars = "Wars",
+  //!Search Strings
+  searchDeepIntoTheHistory = "Search deep into the history", recent = "Recent",
+  //!Profile Strings
+  profile = "Profile", account = "Account", editProfile = "Edit Profile", notification = "Notification", general = "General", settings = "Settings", security = "Security", privacyPolicy = "Privacy Policy", logOut = "Log Out",
+  //!Bazar
+  bazar = "Bazar", historicalPeriods = "Historical periods", historicalCharacters = "Historical Characters", historyBooks = "History Books", historicalSouvenirs = "Historical Souvenirs",
+  //!Cart
+  myCart = "My Cart", totalPayment = "Total Payment", checkoutNow = "Checkout Now",
+  //!Payment Strings
+  checkoutScreen = "Checkout Screen", deliveryAddress = "Delivery Address", change = "Change", items = "1 items", selectedProduct = "Selected Product", paymentMethod = "Payment Method", confirmPayment = "Confirm Payment", orderSuccess = "Order Success", thankYou = "Thank you for your order here and your package will be sent to your address very quickly and fast good product", backToHome = "Back To Home",
+  //!Forgot password
+  forgotPasswordPage = "Forgot Password", sendResetPasswordLink = "Send Reset Passwrod Link", verificationNow = "Verification Now", resendCode = "Resend Code", verifyAccount = "Verify Account", enter4DigitCodeWeHaveSentTo = "Enter 4 digit code we have sent to ", haventReceivedVerificationCode = "Haven’t received verification code?";
+}
+
+abstract class FireBaseStrings {
+  static const String historicalPeriods = "historical_periods";
+  static const String name = "name";
+  static const String description = "description";
+  static const String image = "image";
+  static const String wars = "wars";
 }
