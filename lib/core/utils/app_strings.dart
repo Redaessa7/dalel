c42996dd060a412f1dd75061803f3666c2de465a
Redaessa7 <redaesssa@gmail.com>
2025-08-25 16:14:28 +0300

    add router and app strings

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,21 @@
+abstract class AppStrings {
+  static const String appName = "Dalel";
+  static const String next = "Next";
+  static const String skip = "Skip";
+  static const String createAccount = "Create Account";
+  static const String signUp = "Sign Up";
+  static const String signIn = "Sign In";
+  static const String alreadyHaveAnAccount = "Already have an account ?";
+  static const String forgotPassword = "Forgot Password";
+  static const String verifyAccount = "Verify Account";
+  static const String searchHistory = "Search deep into the history";
+  static const String profile = "Profile";
+  static const String editProfile = "Edit Profile";
+  static const String notification = "Notification";
+  static const String settings = "Settings";
+  static const String general = "General";
+  static const String account = "Account";
+  static const String logOut = "Log Out";
+  static const String privacyPolicy = "Privacy Policy";
+  static const String security = "Security";
+}
